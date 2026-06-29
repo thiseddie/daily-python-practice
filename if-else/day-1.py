12cb749723ae4ae3a040830d3e196eea966af8d9
harsh singh <harsh.pratap.singh2205@gmail.com>
2026-06-29 21:46:20 +0530

    Create day-1.py

---
+++ if-else/day-1.py
@@ -0,0 +1,21 @@
+#1. even odd
+
+
+nums=int(input("enter the number:"))
+
+if  nums % 2 ==0:
+    print("even")
+else:
+    print("odd")
+
+
+#2. positive negative or zero 
+
+nums = int(input("enter the number:"))
+
+if nums >0 :
+    print("the entered number is positive")
+elif nums <0 :
+    print("the number entered is zero")
+else:
+    print("the number entered is zero")
