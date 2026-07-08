c1a4be46d26ccedc3ba3f123600db6ee7f6cbb5f
harsh singh <harsh.pratap.singh2205@gmail.com>
2026-07-08 21:41:48 +0530

    Update if-else-hard.py

--- if-else/if-else-hard.py
+++ if-else/if-else-hard.py
@@ -3,3 +3,9 @@
 
 username = input("enter the username :")
 password = input("enter the password :")
+
+
+if username == "admin" and password == "python123" :
+    print("login successful")
+elif username == "admin" and password ! == "pyhon123" :
+  print("passowrd is incorrect")
