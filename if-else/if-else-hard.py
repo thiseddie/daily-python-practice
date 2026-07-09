4d557079b69f81342344add6205a7de1c8b61859
harsh singh <harsh.pratap.singh2205@gmail.com>
2026-07-09 23:24:31 +0530

    Fix typos in username and password validation

--- if-else/if-else-hard.py
+++ if-else/if-else-hard.py
@@ -11,3 +11,6 @@ elif username == "admin" and password ! == "pyhon123" :
   print("passowrd is incorrect")
 elif username ! == "admin" and password == "pyhton123" :
     print("username is incorrect")
+
+
+#2. electricity bill calculator
