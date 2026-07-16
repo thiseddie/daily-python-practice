18a9f7f7e3728f1e6957d52c933c75489ce6bd7b
harsh singh <harsh.pratap.singh2205@gmail.com>
2026-07-16 22:52:44 +0530

    Update if-else-hard+.py

--- if-else/if-else-hard+.py
+++ if-else/if-else-hard+.py
@@ -5,4 +5,10 @@ import random
 number = random.randint(1, 10)
 
 while true;
-   
+   guess = int(input("enter your guess :"))
+
+   if guess < number :
+      print("too low ")
+   elif guss > number :
+      print("too high")
+      
